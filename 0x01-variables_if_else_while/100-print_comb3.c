3ca7ae93f0dfcb182aec0c89923d13570e577eb3
samuelkchris <samuelkchris@gmail.com>
2022-09-09 04:24:54 -0700

    my commit

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -5,21 +5,24 @@
  * Return: Always 0 (success)
  */
 int main(void)
-{  
+{
 int n, m;
 for (n = 48; n <= 57; n++)
 {
 for (m = 48; m <= 57; m++)
-{	  
+{
+if (m > n)
+{
 putchar(n);
-putchar(m);	  
+putchar(m);
 if (n != 57 || m != 57)
 {
 putchar(',');
 putchar(' ');
 }
 }
-}     
+}
+}
 putchar('\n');
 return (0);
 }
