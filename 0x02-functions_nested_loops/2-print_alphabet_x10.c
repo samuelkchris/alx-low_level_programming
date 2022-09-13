871cebdc79bab462d360517bba5e6748db7cef68
samuelkchris <samuelkchris@gmail.com>
2022-09-13 07:19:29 -0700

    my commit

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,15 +8,15 @@ void print_alphabet_x10(void)
 {
 char ch;
 int i;
-i = 0;  
+i = 0;
 while (i < 10)
 {
-ch = 'a';      
+ch = 'a';
 while (ch <= 'z')
 {
-_putchar(ch);	  
+_putchar(ch);
 ch++;
-}	  
+}
 _putchar('\n');
 i++;
 }
