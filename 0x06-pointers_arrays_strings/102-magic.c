1c397a385c73365fcdf4cb58524c2006ff054324
samuelkchris <samuelkchris@gmail.com>
2022-09-22 15:33:53 -0700

    my commit

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,23 +1,13 @@
 #include <stdio.h>
 
-
-
 int main(void)
   
 {
-  
   int n;
-  
   int a[5];
-  
   int *p;
-  
-
-  
   a[2] = 1024;
-  
   p = &n;
-  
   /*
 
    * write your line of code here...
@@ -39,7 +29,6 @@ int main(void)
   /* ...so that this prints 98\n */
   
   printf("a[2] = %d\n", a[2]);
-  
   return (0);
   
 }
