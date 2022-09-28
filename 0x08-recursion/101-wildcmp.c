d1c42ef14020c1a5d36893179a8a7e05fa625426
samuelkchris <samuelkchris@gmail.com>
2022-09-28 04:02:27 -0700

    my commit

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * wildcmp - compares two strings.
+ * wildcmp-compares two strings.
  * @s1: string 1.
  * @s2: string 2. It can contains a * as a special character.
  * Return: 1 if are identical, 0 if not.
