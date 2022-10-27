2d46f6dc2940760588a72ce30a2f8484d22ba06a
samuelkchris <samuelkchris@gmail.com>
2022-10-27 14:19:28 -0700

    initial commit

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,7 +1,7 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
- * print_binary - prints the binary equivalent of a decimal number
+ * print_binary - print binary equivalent of a decimal number
  * @n: number to print in binary
  */
 void print_binary(unsigned long int n)
