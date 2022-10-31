1121bb45d467345948126ff6c72b7c04494c04ce
Samuel Ssekizinvu <73385715+samuelkchris@users.noreply.github.com>
2022-10-31 21:58:47 +0300

    fix

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,59 +1,44 @@
-#include "main.h"
-
 /**
  * File: 100-elf_header.c
+ * Auth: Brennan D Baraban
  */
+
+#include <sys/elf.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+void check_elf(unsigned char *e_ident);
+void print_magic(unsigned char *e_ident);
+void print_class(unsigned char *e_ident);
+void print_data(unsigned char *e_ident);
+void print_version(unsigned char *e_ident);
+void print_abi(unsigned char *e_ident);
+void print_osabi(unsigned char *e_ident);
+void print_type(unsigned int e_type, unsigned char *e_ident);
+void print_entry(unsigned long int e_entry, unsigned char *e_ident);
+void close_elf(int elf);
 
 /**
- * check_elf - checks if a file is an ELF file
- * @e_ident: pointer to an array
- * Description: if the file is 
+ * check_elf - Checks if a file is an ELF file
+ * @e_ident: A pointer to an array containing the ELF magic numbers.
+ *
+ * Description: if the file is not an ELF file - exit code 98
  */
-
 void check_elf(unsigned char *e_ident)
 {
-	int i;
+	int index;
 
-	for (i = 0; i < 4; i++)
-	{
-		if (e_ident[i] != 127 && e_ident[i] != 'E' && e_ident[i] != 'L' && e_ident[i] != 'F')
+	for (index = 0; index < 4; index++)
+		if (e_ident[index] != 127 &&
+				e_ident[index] != 'E' &&
+				e_ident[index] != 'L' &&
+				e_ident[index] != 'f')
 		{
-			dprintf(STDERR_FILENO, "Error: Not an ELF file\n");
+			dprint(STDERR_FILENO, "Error: Not an ELF file\n");
 			exit(98);
 		}
-	}
 }
-
-/**
- * print_magic - prinrs the magic numbers
- * @e_ident: pointer
- * Description: magic no
- */
-
-void print_magic(unsigned char *e_ident)
-{
-	int x;
-
-	printf(" Magic:  ");
-
-	for (x = 0; x < EI_NIDENT; x++)
-	{
-		printf("%02x", e_ident[x]);
-
-		if (x == EI_NIDENT - 1)
-			printf("\n");
-		else
-			printf(" ");
-	}
-}
-
-/**
- * print_class - prints the class of ELF header
- * @e_ident: a pointer to an array
- */
-
-void print_class(unsigned char *e_ident)
-{
-	printf("  Class:
-
-			switch
