a2416bc6ddebf85a08a9fc10cbcccc24e4c276f6
Samuel Ssekizinvu <73385715+samuelkchris@users.noreply.github.com>
2022-12-08 18:06:49 +0300

    test file

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,25 +1,24 @@
 #include "lists.h"
 
 /**
- * print_dlistint - count number of nodes
- * @h: head of double linked list
- * Return: number of nodes, or NULL if failed
+ * print_dlistint - prints a doubly linked list
+ * @h: pointer to the list
+ *
+ * Return: number of nodes in the list
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-	size_t n = 0;
+	size_t nodes = 0;
 
-	if (h == NULL)
+	if (!h)
 		return (0);
 
-	while (h->prev != NULL)
-		h = h->prev;
-
-	while (h != NULL)
+	while (h)
 	{
 		printf("%d\n", h->n);
-		n++;
 		h = h->next;
+		nodes++;
 	}
-	return (n);
+
+	return (nodes);
 }
